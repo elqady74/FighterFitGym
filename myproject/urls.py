4c38c8ec2c4c90d91a416e30190b5e029f49b0b2
Ahmed Elqady <154700846+elqady74@users.noreply.github.com>
2025-05-12 22:14:35 +0300

    Add files via upload

---
+++ myproject/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+from django.views.generic import TemplateView
+
+urlpatterns = [
+    path('', TemplateView.as_view(template_name='index.html'), name='index'),  # الصفحة الرئيسية
+    path('signup/', TemplateView.as_view(template_name='signup.html'), name='signup'),  # صفحة التسجيل
+    path('api/', include('gym_app.urls')),  # مسارات الـ API
+]
